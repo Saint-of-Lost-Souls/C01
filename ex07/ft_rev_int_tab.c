64f9bbf46f45477eabdb9319cf98212d1c6a448f
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-02 19:43:49 +1030

    Working on it

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -2,20 +2,34 @@
 #include <stdio.h>
 
 void    ft_rev_int_tab(int *tab, int size)
-{
-    
-    while(tab[size])
-    {        
-        size--;        
-    } 
-    printf("The array backwards is: %d", tab[size]);
+{   
+    int temp;
+    int counter;
+
+          
 }
 
 int main()
 {   
-    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; 
-    int tab;
-    ft_rev_int_tab(&tab, 10);    
+    int tab[size = 10] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; 
+    
+    size - 1 = temp;
+    // take 1 put in temp
+    // take 9 put in 1
+    temp put in 9
+    // take 2 put in temp
+    // take 8 put in 2
+    temp put in 8
+    // take 3 put in temp
+    // take 7 put in 3
+    temp put in 7
+    // take 4 put in temp
+    // take 6 put in 4
+    temp put in 6
+    
+    // outcome {9 8 7 6 5 4 3 2 1} 
+    
+    ft_rev_int_tab(&tab, 9);    
 
     return 0;
 }
